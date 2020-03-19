cf0084f7b94d205377c00b15f2eb330645fb1406
Vaibhav Thapliyal <vaibhav.thapliyal6@gmail.com>
2020-03-19 04:11:48 -0700

    Implemented validations for blogs, skills and portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,4 @@
 class Portfolio < ApplicationRecord
+
+	validate_presence_of :title, :body, :main_image, :thumb_image
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+
+	validate_presence_of :title, :percent_utilized
 end
